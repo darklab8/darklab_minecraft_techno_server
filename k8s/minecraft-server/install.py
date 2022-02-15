0c2b53fb147ecc3f8ef310bc05e1f75118a3cd32
dd84ai <dd84ai@gmail.com>
2022-02-15 20:12:52 +0700

    feat: limit to job history

--- k8s/minecraft-server/install.py
+++ k8s/minecraft-server/install.py
@@ -2,9 +2,9 @@ import os
 import argparse
 
 my_parser = argparse.ArgumentParser(description='')
-my_parser.add_argument('--dockerconfig',
+my_parser.add_argument('--dockerconfigjson',
                        type=str,
-                       help='dockerconfig') 
+                       help='dockerconfigjson') 
 args = my_parser.parse_args()
 
 def shell(cmd):
@@ -15,4 +15,4 @@ def shell(cmd):
         exit(status_code)
 
 
-shell(f'helm upgrade --install --create-namespace --namespace minecraft-techno-prod minecraft-server . --set dockerconfigjson={args.dockerconfig}')
+shell(f'helm upgrade --install --create-namespace --namespace minecraft-techno-prod minecraft-server . --set dockerconfigjson={args.dockerconfigjson}')
